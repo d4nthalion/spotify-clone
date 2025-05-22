9796d3aac3314c9a785c363fb7529f69145a0a0f
Carlos Bermúdez <carlosbermudezmancebo@gmail.com>
2025-05-22 09:12:42 +0200

    modules/shared/media-player/: Media player modified

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-media-player',
@@ -6,6 +6,15 @@ import { Component } from '@angular/core';
   templateUrl: './media-player.component.html',
   styleUrl: './media-player.component.css'
 })
-export class MediaPlayerComponent {
+export class MediaPlayerComponent implements OnInit{
+  mockCover:any = {
+    cover: 'https://i.scdn.co/image/ab67616d0000b27320292e6cce666a69ba5a86fb',
+    album: 'Metallica',
+    name: 'Seek & Destroy'
+  }
+  constructor() {}
 
+  ngOnInit(): void {
+      
+  }
 }
